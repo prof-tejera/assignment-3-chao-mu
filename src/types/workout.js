b6fd9ac7ce29a311d8aa1f96cf255167147dbb6d
Danimal <danimal@hackpoetic.com>
2023-12-19 13:29:23 -0500

    refactor: remove unused files

--- src/types/workout.js
+++
@@ -1,9 +0,0 @@
-/**
- * @typedef {Object} Workout
- * @property {import("@/types/timer").TimerOptions[]} plan
- * @property {import("@/types/timer").TimerOptions | null} currentTimerOptions
- * @property {boolean} completed
- * @property {boolean} isLastTimer
- * @property {boolean} isFirstTimer
- */
-export const Workout = {};
