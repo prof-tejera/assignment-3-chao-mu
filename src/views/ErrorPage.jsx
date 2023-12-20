b59d5c1d922fad865f2c22e0075e171b0f0e9554
Danimal <danimal@hackpoetic.com>
2023-12-20 01:53:20 -0500

    feat: decent error page

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,15 +1,19 @@
 import { useRouteError } from "react-router-dom";
 
+import styles from "./ErrorPage.module.css";
+
 export default function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
+    <main className={styles["error-page"]}>
+      <div className={styles["hgroup"]}>
+        <h1>Oops! :-(</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+      </div>
+      <p className={styles["error"]}>
+        <em>{error.statusText || error.message}</em>
       </p>
-    </div>
+    </main>
   );
 }
