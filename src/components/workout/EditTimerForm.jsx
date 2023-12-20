4b4d3355dfea5166254ac2e33199a4158ea03f0c
Danimal <danimal@hackpoetic.com>
2023-12-20 00:39:38 -0500

    refactor: Create edit container form

---
+++ src/components/workout/EditTimerForm.jsx
@@ -0,0 +1,40 @@
+// react-router-dom
+import { useNavigate } from "react-router-dom";
+
+// Ours - Utils
+import throwNotFound from "@/utils/throwNotFound";
+
+// Ours - Contexts
+import { useWorkoutContext } from "@/contexts/WorkoutContext";
+import { useWorkoutDispatchContext } from "@/contexts/WorkoutContext";
+
+// Ours - Components
+import TimerForm from "@/components/timer/TimerForm";
+
+// Ours - Reducers
+import { WorkoutActionType } from "@/reducers/workoutReducer";
+
+const EditPage = ({ timerId }) => {
+  const navigate = useNavigate();
+
+  const { plan } = useWorkoutContext();
+  const workoutDispatch = useWorkoutDispatchContext();
+
+  const timerOptions = plan.find((timer) => timer.id === timerId);
+  if (timerOptions === undefined) {
+    throwNotFound(`Timer with id ${timerId} not found`);
+  }
+
+  // Update the timer and redirect to home
+  const handleSubmit = (options) => {
+    workoutDispatch({
+      type: WorkoutActionType.UPDATE_TIMER,
+      payload: { options },
+    });
+    navigate("/");
+  };
+
+  return <TimerForm values={timerOptions} onSubmit={handleSubmit} />;
+};
+
+export default EditPage;
--- src/views/EditPage.jsx
+++ src/views/EditPage.jsx
@@ -1,46 +1,18 @@
 // react-router-dom
-import { useNavigate, useParams } from "react-router-dom";
-
-// Ours - Utils
-import throwNotFound from "@/utils/throwNotFound";
-
-// Ours - Contexts
-import { useWorkoutContext } from "@/contexts/WorkoutContext";
-import { useWorkoutDispatchContext } from "@/contexts/WorkoutContext";
+import { useParams } from "react-router-dom";
 
 // Ours - Components
-import TimerForm from "@/components/timer/TimerForm";
+import EditTimerForm from "@/components/workout/EditTimerForm";
 
 // Ours - Styles
 import styles from "./EditPage.module.css";
 
-// Ours - Reducers
-import { WorkoutActionType } from "@/reducers/workoutReducer";
-
 const EditPage = () => {
-  const navigate = useNavigate();
   const { timerId } = useParams();
-
-  const { plan } = useWorkoutContext();
-  const workoutDispatch = useWorkoutDispatchContext();
-
-  const timerOptions = plan.find((timer) => timer.id === timerId);
-  if (timerOptions === undefined) {
-    throwNotFound(`Timer with id ${timerId} not found`);
-  }
-
-  // Update the timer and redirect to home
-  const handleSubmit = (options) => {
-    workoutDispatch({
-      type: WorkoutActionType.UPDATE_TIMER,
-      payload: { options },
-    });
-    navigate("/");
-  };
 
   return (
     <section className={styles["add-page"]}>
-      <TimerForm values={timerOptions} onSubmit={handleSubmit} />
+      <EditTimerForm timerId={timerId} />
     </section>
   );
 };
